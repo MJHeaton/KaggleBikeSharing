549004a2f8d44e327bb1cc18e23d6f5062e6b3ea
Matthew Heaton <mheaton@MattHeatonMBP.app.byu.edu>
2021-01-12 15:00:16 -0700

    Input libraries I need for analysis

--- DataCleaning.R
+++ DataCleaning.R
@@ -1,3 +1,13 @@
-## This is my R File
+##
+## R Code for Bike Sharing Kaggle Competition
+##
 
+## Libraries I need
 library(tidyverse)
+library(caret)
+library(DataExplorer)
+
+## Read in the data
+
+
+
